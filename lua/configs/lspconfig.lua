6f5292d290dfae26d396e2788d0e4a968061f467
Zacharias-Brohn <zacbro-8@student.ltu.se>
2025-01-11 14:31:30 +0100

    revert

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,88 +1,78 @@
-local M = {}
-local map = vim.keymap.set
+local cmp = require("cmp")
+local cmp_lsp = require("cmp_nvim_lsp")
+local capabilities = vim.tbl_deep_extend(
+  "force",
+  {},
+  vim.lsp.protocol.make_client_capabilities(),
+  cmp_lsp.default_capabilities()
+)
 
--- export on_attach & capabilities
-M.on_attach = function(_, bufnr)
-  local function opts(desc)
-    return { buffer = bufnr, desc = "LSP " .. desc }
-  end
 
-  map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
-  map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
-  map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
-  map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
-
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts "List workspace folders")
-
-  map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
-  map("n", "<leader>ra", require "nvchad.lsp.renamer", opts "NvRenamer")
-
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Code action")
-  map("n", "gr", vim.lsp.buf.references, opts "Show references")
-end
-
--- disable semanticTokens
-M.on_init = function(client, _)
-  if client.supports_method "textDocument/semanticTokens" then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
-end
-
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-
-M.capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { "markdown", "plaintext" },
-  snippetSupport = true,
-  preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
-  commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
-  resolveSupport = {
-    properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
-    },
-  },
-}
-
-M.defaults = function()
-  dofile(vim.g.base46_cache .. "lsp")
-  require("nvchad.lsp").diagnostic_config()
-
-  require("lspconfig").lua_ls.setup {
-    on_attach = M.on_attach,
-    capabilities = M.capabilities,
-    on_init = M.on_init,
-
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            vim.fn.expand "$VIMRUNTIME/lua",
-            vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-            vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-            vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-            "${3rd}/luv/library",
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-      },
-    },
-  }
-end
-
+require("nvchad.configs.lspconfig").defaults()
 require("fidget").setup({})
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "rust_analyzer",
+    "gopls",
+  },
+  handlers = {
+    function(server_name) -- default handler (optional)
+      require("lspconfig")[server_name].setup {
+        capabilities = capabilities
+      }
+    end,
+
+    zls = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.zls.setup({
+        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+        settings = {
+          zls = {
+            enable_inlay_hints = true,
+            enable_snippets = true,
+            warn_style = true,
+          },
+        },
+      })
+      vim.g.zig_fmt_parse_errors = 0
+      vim.g.zig_fmt_autosave = 0
+
+    end,
+    ["lua_ls"] = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = { version = "Lua 5.1" },
+            diagnostics = {
+              globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+            }
+          }
+        }
+      }
+    end,
+  }
+})
+
+local cmp_select = { behaivior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  },
+  {
+    { name = 'buffer' },
+  })
+})
 
 vim.diagnostic.config({
   -- update_in_insert = true,
@@ -97,6 +87,7 @@ vim.diagnostic.config({
 })
 
 local lspconfig = require "lspconfig"
+local nvlsp = require "nvchad.configs.lspconfig"
 
 -- EXAMPLE
 local servers = {
@@ -114,15 +105,15 @@ local servers = {
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = M.on_attach,
-    on_init = M.on_init,
-    capabilities = M.capabilities,
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
   }
 end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
---   on_attach = M.on_attach,
---   on_init = M.on_init,
---   capabilities = M.capabilities,
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
 -- }
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,9 +1,34 @@
+-- local function resize_left()
+--   local cur_win = vim.api.nvim_get_current_win()
+--   vim.cmd('wincmd h')
+--   local new_win = vim.api.nvim_get_current_win()
+--   if cur_win == new_win then
+--     vim.cmd('vertical resize -10')
+--   elseif vim.bo[new_win].filetype == 'NvimTree' then
+--
+--   else
+--     vim.cmd('vertical resize -10')
+--     vim.cmd('wincmd l')
+--   end
+-- end
+--
+-- local function resize_right()
+--   local cur_win = vim.api.nvim_get_current_win()
+--   vim.cmd('wincmd l')
+--   local new_win = vim.api.nvim_get_current_win()
+--   if cur_win == new_win then
+--     vim.cmd('vertical resize -10')
+--   else
+--     vim.cmd('vertical resize -10')
+--     vim.cmd('wincmd h')
+--   end
+-- end
+
 require "nvchad.mappings"
 
-local map = vim.keymap.set
+-- add yours here
 
-map("v", "S-Up", ":m '<-2<CR>gv=gv")
-map("v", "S-Down", ":m '>+1<CR>gv=gv")
+local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -24,3 +49,5 @@ map('n', '<leader>u', vim.cmd.UndotreeToggle)
 map("n", "<A-->", "<cmd>BufDel<cr>")
 
 map("n", "<C-a>", "ggVG", { noremap = true, silent = true })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
