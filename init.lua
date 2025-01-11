c864505309d736ebf02634c52572a1f15eee71c4
Zacharias-Brohn <zacbro-8@student.ltu.se>
2025-01-11 13:38:31 +0100

    test

--- init.lua
+++ init.lua
@@ -1,15 +1,20 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
-vim.cmd("source test.vim")
+vim.cmd "source test.vim"
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 if not vim.uv.fs_stat(lazypath) then
-    local repo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system {
-        "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath
-    }
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    repo,
+    "--branch=stable",
+    lazypath,
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -18,14 +23,7 @@ local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-    {
-        "NvChad/NvChad",
-        lazy = false,
-        branch = "v2.5",
-        import = "nvchad.plugins",
-    },
-
-    { import = "plugins" },
+    import = "plugins"
 }, lazy_config)
 
 -- load theme
@@ -33,12 +31,13 @@ dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
 require "options"
+require "plugins"
 require "nvchad.autocmds"
 
-require("configs.set")
+require "configs.set"
 
 vim.opt.relativenumber = true
 
 vim.schedule(function()
-    require "mappings"
+  require "mappings"
 end)
