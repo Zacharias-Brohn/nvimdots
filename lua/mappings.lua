22de8b46745095af676e58ac9ac7aef0ef5bc19a
Zacharias-Brohn <zacbro-8@student.ltu.se>
2025-01-22 14:45:27 +0100

    move selected text

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,19 +1,18 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
+-- Move selected text up
+map("v", "<C-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selected text up" })
+map("v", "<C-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selected text down" })
 
 -- Alt + Arrow Key to change buffer
 map("n", "<A-Left>", "<C-w>h", { desc = "Move to left split" })
 map("n", "<A-Down>", "<C-w>j", { desc = "Move to bottom split" })
 map("n", "<A-Up>", "<C-w>k", { desc = "Move to top split" })
 map("n", "<A-Right>", "<C-w>l", { desc = "Move to right split" })
-
--- Copilot suggestion
--- map("i", "jk", vim.cmd("Copilot suggestion accept_word"), { expr = true })
 
 -- Copilot Chat buffer
 map("n", "<A-c>", vim.cmd.CopilotChatToggle)
@@ -25,5 +24,3 @@ map('n', '<leader>u', vim.cmd.UndotreeToggle)
 map("n", "<A-->", "<cmd>BufDel<cr>")
 
 map("n", "<C-a>", "ggVG", { noremap = true, silent = true })
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
